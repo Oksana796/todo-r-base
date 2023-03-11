61534940c5f62b631f9ecf679a799af1be2e2b52
Oksana <ksenia79460@gmail.com>
2023-03-11 16:16:01 +0000

    add, render task, clean input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import React, { useState } from "react";
 
+let id = 0;
+let INIT = [
+  { id: id++, item: "Buy a flat" },
+  { id: id++, item: "Buy a car" },
+  { id: id++, item: "Find a job" },
+];
+
 function App() {
-  return <div className="App"></div>;
+  const [newTask, setNewTask] = useState("");
+  const [list, setList] = useState(INIT);
+
+  const addNewTask = (e) => {
+    e.preventDefault();
+    setList(() => list.concat({ id: id++, item: newTask }));
+    setNewTask("");
+  };
+
+  return (
+    <div className="App">
+      <h1>ToDo</h1>
+      <div>
+        <input
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          type="text"
+          placeholder="Add new task"
+        />
+        <button onClick={addNewTask}>Add</button>
+      </div>
+      <div>
+        <ul>
+          {list.map(({ id, item }) => (
+            <li key={id}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
